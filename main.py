755f43156d4b3920935093d33777d896400078b4
humcio <koopa678@gmail.com>
2025-03-07 18:00:16 +0100

    added player movement, asteroids, spawn logic

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import pygame
 from constants import *
+from player import *
+from asteroids import *
+from asteroidfield import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroidsGroup = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroids.containers = (asteroidsGroup, drawable, updatable)
+    AsteroidField.containers = (updatable)
+
+
+
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    playerUnit = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    new_field = AsteroidField()
+    #################### MAIN LOOP STARTS HERE ############################
 
     running = True
     while running:
@@ -11,15 +30,19 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0,0,0))
+        
+        for item in drawable:
+            item.draw(screen)
+        updatable.update(dt)
+        #playerUnit.draw(screen)
+        #playerUnit.update(dt)
+
+
         pygame.display.flip()
-
-
-
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
-
-
+        clock.tick(60)
+        dt = clock.tick(60)/1000
+        
+        
 
 
 
