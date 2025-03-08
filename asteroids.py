2a0d388f15cd716440a8ddaf11d0f061b2dbac47
humcio <koopa678@gmail.com>
2025-03-08 23:28:20 +0100

    added shooting, killing, splitting and minor optimizations for no reason

--- asteroids.py
+++ asteroids.py
@@ -1,13 +1,38 @@
 from constants import *
 from circleshape import *
+import random
 
 
 class Asteroids(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.happiness = 30
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius)
 
     def update(self, dt):
         self.position += self.velocity * dt
+        if self.happiness < 0:
+            self.kill()
+        self.happiness -= dt
+
+    def split(self):
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            self.kill()
+            return
+        else:
+            split_angle = random.uniform(20, 50)
+            new_vector1 = self.velocity.rotate(-split_angle)
+            new_vector2 = self.velocity.rotate(split_angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            new_astero1 = Asteroids(self.position.x, self.position.y, new_radius)
+            new_astero2 = Asteroids(self.position.x, self.position.y, new_radius)
+
+            new_astero1.velocity = new_vector1 * 1.2
+            new_astero2.velocity = new_vector2 * 1.2
+
+            self.kill()
+
+
--- player.py
+++ player.py
@@ -1,10 +1,12 @@
 from constants import *
 from circleshape import *
+from shooting import *
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_cooldown = 0
         
 
         # in the player class
@@ -24,6 +26,7 @@ class Player(CircleShape):
     
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shoot_cooldown -= dt
 
         if keys[pygame.K_a]:
             self.rotate(dt*-1)
@@ -33,7 +36,21 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(dt*-1)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+    
+    def shoot(self):
+        if self.shoot_cooldown <= 0:
+            new_shot = Shot(self.position.x, self.position.y,  SHOT_RADIUS)
+            new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.shoot_cooldown = 0.3
+        
+
+
+        
+
+        
